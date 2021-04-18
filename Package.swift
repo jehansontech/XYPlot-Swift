0fda6f2fe3456d162e1adebd8ddc27c15ab21cfa
Jim Hanson <jim@jehanson.org>
2021-04-18 11:34:56 -0400

    rename

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "XYPlot",
+    name: "XYPlot-Swift",
     platforms: [
         .macOS(.v11), .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "XYPlot",
-            targets: ["XYPlot"])
+            name: "XYPlot-Swift",
+            targets: ["XYPlot-Swift"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "UIStuff",
+        .package(name: "UIStuff-Swift",
                  url: "git@github.com:jehansontech/UIStuff-Swift.git",
                  .branch("dev"))
     ],
@@ -25,10 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "XYPlot",
-            dependencies: ["UIStuff"]),
-        .testTarget(
-            name: "XYPlotTests",
-            dependencies: ["XYPlot"]),
+            name: "XYPlot-Swift",
+            dependencies: ["UIStuff-Swift"])
     ]
 )
