478d8199529590c83a4efad7a6f1cf0864e118b4
Jim Hanson <jim@jehanson.org>
2021-05-12 06:58:40 -0400

    d

--- Sources/XYPlotForSwift/CaptionView.swift
+++ Sources/XYPlotForSwift/CaptionView.swift
@@ -12,15 +12,28 @@ struct CaptionView: View {
     @Binding var layer: XYLayer
 
     var body: some View {
-        HStack {
-            VStack {
-                Text("Caption Line 1")
-                Text("Caption Line 2")
-            }
-            VStack {
-                Text("Caption Line 3")
-                Text("Caption Line 4")
-                Text("Caption Line 5")
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                HStack {
+                    Rectangle().frame(width: 50, height: 2)
+                    Text("Caption Line 1")
+                }
+                HStack {
+                    Rectangle().frame(width: 50, height: 2)
+                    Text("Caption Line 2")
+                }
+                HStack {
+                    Rectangle().frame(width: 50, height: 2)
+                    Text("Caption Line 3")
+                }
+                HStack {
+                    Rectangle().frame(width: 50, height: 2)
+                    Text("Caption Line 4")
+                }
+                HStack {
+                    Rectangle().frame(width: 50, height: 2)
+                    Text("Caption Line 5")
+                }
             }
         }
     }
